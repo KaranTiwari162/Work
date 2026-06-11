8c10bb78d62d3509793fa8bb0df60ee683852777
webkaran18 <webdevkaran18@gmail.com>
2026-06-11 22:02:11 +0530

    working on project

--- spotify project/script.js
+++ spotify project/script.js
@@ -1,8 +1,12 @@
 console.log('Lets write JavaScript');
+let currentSong = new Audio();
+let songs;
+let currFolder;
+let play = document.getElementById("play");
 
 async function getsongs(){
 
-    let a = await fetch("http://127.0.0.1:5500/spotify%20project/songs/")
+    let a = await fetch("http://127.0.0.1:5500/songs/")
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -13,25 +17,64 @@ async function getsongs(){
         const element = as[index];
 
         if(element.href.endsWith(".mp3")){
-            songs.push(element.href)
+            songs.push(element.href.split(`/songs/`)[1])
         }
     }
     return songs
  }
 
+ const playMusic =(track)=>{
+    currentSong.src ="/songs/" + track
+    currentSong.play()
+    
+    document.getElementById("play").src = "img/pause.svg";
+ }
+ 
+
+
+//  Get the list of all the songs
+
  async function main(){
+
 let songs= await getsongs()
 console.log(songs)
-let songul = document.querySelector(".songlist").getElementsByTagName("ul")
+
+// Show all the song in the paylist 
+
+let songul = document.querySelector(".songlist").getElementsByTagName("ul")[0]
 for (const song of songs) {
-    
+    songul.innerHTML = songul.innerHTML +  `<li>
+    <img class="invert" src="img/music.svg" alt="">
+    <div class="info">
+    <div> ${song.replaceAll("%20", " ")}</div>
+</div>
+<div class="playnow">
+    <span>Play Now</span>
+    <img class="invert" src="img/play.svg" alt="">
+</div>
+    </li>`;
+   
 }
-var audio = new Audio(songs[1])
-audio.play();
 
-audio.addEventListener("loadeddata", () => {
-    console.log(audio.duration , audio.currentSrc , audio.currentTime);
-});
+// Attach an event listener to each song
+Array.from(document.querySelector(".songlist").getElementsByTagName("li")).forEach(e => {
+    e.addEventListener("click", element => {
+        playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
 
- }
+    })
+})
+
+
+ // Attach an event listener to play, next and previous
+ play.addEventListener("click", () => {
+     if (currentSong.paused) {
+         currentSong.play()
+         play.src = "img/pause.svg"
+     }
+     else {
+         currentSong.pause()
+         play.src = "img/play.svg"
+     }
+ })
+}
  main()
